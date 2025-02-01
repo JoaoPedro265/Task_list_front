92675d0b120b3754a98c7335d72d691d520dbdeb
João pedro oliveira <joaogrilo265@gmail.com>
2025-01-31 23:30:11 -0300

    add error404.css

--- pages/error404.jsx
+++ pages/error404.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import "./components/styles/Loginfield.css";
+import "./error404.css";
 import { Box } from "@mui/material";
 export function Error404() {
   return (
-    <Box className="login-Box">
+    <Box className="error404">
       <h1>404</h1>
       <p>Page not Found.</p>
       <p>Try searching for something else.</p>
